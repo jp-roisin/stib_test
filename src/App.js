7f633dd4788f322d1cdc4d4431071e5a3f05e115
jp-roisin <jeanpaul.roisin@protonmail.com>
2023-05-07 20:44:30 +0200

    Project setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const randomId = "0a70ccc5f0d692ae37c9a15f637cc6261b882530"
+  const datasetsUrl = "https://stibmivb.opendatasoft.com/api/v2/catalog/datasets"
+  const waitingTimeUrl = "https://stibmivb.opendatasoft.com/api/v2/catalog/datasets/waiting-time-rt-production/records/" + randomId;
+
+  const [data, setData] = useState();
+  useEffect(() => {
+    axios.get(waitingTimeUrl, {
+      headers: {
+        // Content_Type: "application/json",
+        // Authorization: "Bearer " + token,
+        Authorization: "Apikey " + process.env.REACT_APP_API_KEY,
+      }
+    })
+      .then(res => {
+        console.log(res.data);
+        setData(res.data);
+      })
+  }, [waitingTimeUrl]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>{data ? data.record.fields.lineid : "no data"}</div>
   );
 }
 
